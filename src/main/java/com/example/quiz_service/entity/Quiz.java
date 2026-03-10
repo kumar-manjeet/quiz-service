540f3c3af35af6de0dc4c4c248c76848df80cebd
kumar-manjeet <manjeetyadav725@gmail.com>
2026-03-10 20:19:41 +0530

    changes

---
+++ src/main/java/com/example/quiz_service/entity/Quiz.java
@@ -0,0 +1,18 @@
+package com.example.quiz_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer totalQuestion;
+}
